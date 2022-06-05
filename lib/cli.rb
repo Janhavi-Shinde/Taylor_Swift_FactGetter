6be11255ed277e611d80db2205b51047fb71e848
janhavi <janashinde394@gmail.com>
2022-06-06 00:00:14 +1000

    semi-prettified welcome mssg to be more readable

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,12 @@
 def welcome
-puts "Welcome to the CLI App!"
-puts "Please select a number between 1-15, to find out fun facts about that song"
+ puts " "
+puts " ***************************"
+puts "      Welcome to the CLI App!"
+puts "      Please select a number between 1-15, to find out fun facts about that song"
+puts " "
+puts " ***************************"
+puts " "
+puts " "
 puts Scraper.title
 end
 
