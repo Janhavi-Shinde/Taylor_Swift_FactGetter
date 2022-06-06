07fa6a6dfc1ee85cc0be28c7c32950a34fe8cec7
janhavi <janashinde394@gmail.com>
2022-06-06 23:56:07 +1000

    cli loop working

--- bin/executable.rb
+++ bin/executable.rb
@@ -1,9 +1,17 @@
 #!/usr/bin/env ruby
 require_relative "../config/environment"
 
-Cli.new
+var_1 = Cli.new
 
+def cli_loop(var_1)
 
+while(true)
+    break if (var_1.input_2 == 'n' || var_1.input_2 =='N')
+    var_1.ask_after_facts  
+    end
+end  
+
+cli_loop(var_1)
 
 # def coverter(song_name)
 #     song_name.gsub(" ","-")
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,6 @@
 require_relative "./scraper.rb"
 class Cli
+    attr_accessor :input_2
 @@scraper
     def initialize
         @@scraper = Scraper.new
@@ -8,12 +9,14 @@ class Cli
         
         welcome
         get_fact
+        
         # puts scraper.converter('end game') ==> is returning: end-game 
     end
     
     def get_fact
         input = gets.chomp.to_i
         @@scraper.scrape_facts(Scraper.title[input - 1])
+        input = nil
     end
 
     def list_of_songs
@@ -26,12 +29,28 @@ class Cli
         puts " "
         puts "      ***************************"
         puts "      Welcome to the CLI App!"
-        puts "      Please select a number between 1-15, to find out fun facts about that song"
+        puts "      Please select a number between 1-16, to find out fun facts about that song"
         puts " "
         puts "      ***************************"
          puts " "
         list_of_songs
     end
 
+    def ask_after_facts
+        puts "          ************************"
+        puts "Would you like to pick another song? Enter either y or n"
+        @input_2 = gets.chomp
+        if @input_2 == 'y' || @input_2 == 'Y'
+            list_of_songs
+            get_fact
+        elsif @input_2 == 'n' || @input_2 == 'N'
+            nil
+        end
+
+    end
+
+    def input_2
+        @input_2
+    end
 end
 
